3f54e516371e9191c1b219c336a3738f59806743
Jame McNeilan <jamesmcneilan@protonmail.com>
2022-06-13 18:45:32 -0600

    Made smooth svg transition for categories

--- public/src/components/products/viewAll/optionsBar.js
+++ public/src/components/products/viewAll/optionsBar.js
@@ -9,15 +9,6 @@ import { FaSearch, FaCheck } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 import { TextInput } from "@mantine/core";
 
-/*
-now that the categories are partially implemented,
-I need to fix the positioning of the search bar
-and i need to convert the products page initial load,
-to use the new getProducts one
-then i can get rid of the GET request one on the front and back
-
-*/
-
 const OptionsBar = ({ queryString = "" }) => {
   // Set a search term and toggle-able item categories
   const [keyword, setKeyword] = useState(queryString);
@@ -49,8 +40,8 @@ const OptionsBar = ({ queryString = "" }) => {
   return (
     <form
       onSubmit={onSearch}
-      className={`mx-auto relative mt-6 mb-12 flex flex-row lg:flex-nowrap
-        flex-wrap items-center justify-center lg:h-20 w-162`}
+      className={`mx-auto mt-6 mb-12 flex items-center 
+      justify-center flex-wrap md:flex-nowrap`}
     >
       <TextInput
         placeholder="Search"
@@ -58,7 +49,7 @@ const OptionsBar = ({ queryString = "" }) => {
         value={keyword}
         radius="md"
         size="xs"
-        className="lg:ml-29 w-44"
+        className="w-44 -mr-2"
         onChange={(e) => setKeyword(e.target.value)}
       />
 
@@ -66,75 +57,79 @@ const OptionsBar = ({ queryString = "" }) => {
         type="submit"
         style={{ background: "radial-gradient(at center, #789, #345)" }}
         className={`z-10 p-4 text-yellow-500 rounded-full transform duration-150
-          hover:scale-105 lg:absolute border-2 border-yellow-600`}
+          hover:scale-105 border-2 border-yellow-600`}
       >
         <FaSearch size="25" />
       </button>
 
-      <div
-        className={`flex flex-row flex-wrap items-center justify-center
-        lg:mt-0 mt-4 lg:w-auto w-full`}
-      >
-        {/* trapezoid to connect the search button and category options */}
-        <div className="hidden lg:flex h-12 w-14 flex-col">
-          <svg
-            className={"h-4"}
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            fill="inherit"
-          >
-            <polygon fill="rgb(55 65 81)" points="100,0 0,100 100,100" />
-          </svg>
-          <div className="flex-grow bg-gray-700" />
-          <svg
-            className={"h-4"}
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            fill="inherit"
-          >
-            <polygon fill="rgb(55 65 81)" points="100,0 0,0 100,100" />
-          </svg>
-        </div>
+      <div className="-ml-1 md:mt-0 mt-4 w-full md:w-auto">
+        <div className={`flex flex-row items-center justify-center w-full`}>
+          {/* trapezoid to connect the search button and category options */}
+          <div className="flex flex-col h-12 md:w-14 w-0 ">
+            <svg
+              className={"h-4 w-full"}
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M0,100 C50,100 50,0 100,0 L 100,100 Z"
+                fill="rgb(55 65 81)"
+              />
+            </svg>
+            <div className="h-full bg-gray-700" />
+            <svg
+              className={"h-4 w-full"}
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M0,0 C50,0 50,100 100,100 L 100,0 Z"
+                fill="rgb(55 65 81)"
+              />
+            </svg>
+          </div>
 
-        <div
-          className={` h-12 flex flex-row p-2 rounded-lg lg:rounded-l-none
-          bg-gray-700`}
-        >
-          {catArr.map((cat, index) => {
-            return (
-              <button
-                key={index}
-                type="button"
-                name={cat}
-                onClick={() => onCategoryToggle(cat)}
-                style={{ background: "radial-gradient(at center, #eee, #cde)" }}
-                className={`flex items-center justify-center px-2 py-4
+          <div
+            className={` h-12 grid grid-cols-3 p-2 rounded-lg md:rounded-l-none bg-gray-700`}
+          >
+            {catArr.map((cat, index) => {
+              return (
+                <button
+                  key={index}
+                  type="button"
+                  name={cat}
+                  onClick={() => onCategoryToggle(cat)}
+                  style={{
+                    background: "radial-gradient(at center, #eee, #cde)",
+                  }}
+                  className={`h-8 flex items-center justify-center px-2 py-4
                     rounded-md transform duration-150 hover:scale-105
                     ${index !== catArr.length - 1 && "mr-2"}`}
-              >
-                <p
-                  className={`text-lg font-semibold z-0
-                    ${!categories[cat] && "opacity-50"}`}
                 >
-                  {cat.substring(0, 1).toUpperCase() +
-                    cat.substring(1, cat.length) +
-                    "s"}
-                </p>
-                <FaCheck
-                  style={{ top: 2 + "px", right: 3 + "px" }}
-                  size="10"
-                  className={`${!categories[cat] && "hidden"}
+                  <p
+                    className={`text-lg font-semibold z-0
+                    ${!categories[cat] && "opacity-50"}`}
+                  >
+                    {cat.substring(0, 1).toUpperCase() +
+                      cat.substring(1, cat.length) +
+                      "s"}
+                  </p>
+                  <FaCheck
+                    style={{ top: 2 + "px", right: 3 + "px" }}
+                    size="10"
+                    className={`${!categories[cat] && "hidden"}
                     text-green-500 z-10 absolute`}
-                />
-                <ImCross
-                  style={{ top: 2 + "px", right: 3 + "px" }}
-                  size="10"
-                  className={`${categories[cat] && "hidden"}
+                  />
+                  <ImCross
+                    style={{ top: 2 + "px", right: 3 + "px" }}
+                    size="10"
+                    className={`${categories[cat] && "hidden"}
                     text-red-400 z-10 absolute`}
-                />
-              </button>
-            );
-          })}
+                  />
+                </button>
+              );
+            })}
+          </div>
         </div>
       </div>
     </form>
